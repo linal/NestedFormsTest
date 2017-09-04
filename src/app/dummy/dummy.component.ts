d5305d3c21993e9246882b7b9a2e029751ce0d3b
John Tindell <john@yeticode.co.uk>
2017-09-04 23:17:03 +0100

    updated stuff

--- src/app/dummy/dummy.component.ts
+++ src/app/dummy/dummy.component.ts
@@ -11,10 +11,22 @@ export class DummyComponent implements OnInit {
   constructor() { }
 
   @Input('group')
-  private dummyGroup: FormGroup;  
+  private dummyGroup: FormGroup;
 
   ngOnInit() {
     var control = new FormControl('', [Validators.required]);
     this.dummyGroup.addControl('dummyInput', control);
+  }
+
+  click() {
+    console.log("click()");
+  }
+
+  fileChange(event) {
+    let fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+      let file: File = fileList[0];
+      console.log(file.name);
+    }
   }
 }
